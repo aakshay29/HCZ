54592271c2bc9ba61d112e2f2eca57dc522e8a48
jamwal89 <jamwal89@github.com>
2016-10-20 16:54:55 -0400

    new

--- src/Util/HCZJobUtil.java
+++ src/Util/HCZJobUtil.java
@@ -1,5 +1,30 @@
 package Util;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import UserData.DBUtil;
+
+import model.Hczjob;
+
 public class HCZJobUtil {
 
+	public static List<Hczjob> getJobs() {
+		EntityManager em = DBUtil.getEmFactory().createEntityManager();
+		String qString = "Select u from Hczjob u";
+		TypedQuery<Hczjob> q = em.createQuery(qString, Hczjob.class);
+		List<Hczjob> jobs = null;
+		try {
+			jobs = q.getResultList();
+		} catch (NoResultException e) {
+			System.out.println(e);
+		} finally {
+			em.close();
+		}
+		return jobs;
+		
+	}
 }
